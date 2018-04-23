56202d896d105c8d4e1a99e7d3a20e860909f036
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsApplication3/MyTreeList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DevExpress.Skins;
+using DevExpress.XtraTreeList.ViewInfo;
+using DevExpress.XtraTreeList;
+using System.Drawing;
+
+namespace DXSample {
+    public class MyTreeList : TreeList
+    {
+        public MyTreeList()
+            : base()
+        {
+            this.OptionsView.ShowIndicator = false;
+            this.OptionsView.ShowFirstLines = false;
+            this.OptionsView.ShowVertLines = false;
+        }
+        public MyTreeList(object ignore) : base(ignore) { }
+
+        protected override TreeListViewInfo CreateViewInfo()
+        {
+            return new MyTreeListViewInfo(this);
+        }
+    }
+
+    public class MyTreeListViewInfo : TreeListViewInfo
+    {
+        public MyTreeListViewInfo(MyTreeList tree)
+            : base(tree)
+        {
+
+        }
+
+        protected override void CalcRowIndentInfo(RowInfo ri) {
+        }
+        protected override int GetViewLevel(int level) {
+            return 0;
+        }
+    }
+}
